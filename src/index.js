10bcfbb14d7457352f4dc3a29654edf49ee37254
nina <ninagbs@abv.bg>
2023-06-14 17:54:21 +0300

    add and setup handlebras

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const app = express();
+const handlebars = require('express-handlebars')
+
 const routes = require('./routes')
 
+
+const app = express();
+
+
+// Config handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs');
+// only with src-fplder
+app.set('views', 'src/views');
 
 
 app.use(express.static('public'));
