228d36b2a346202c4e381976ae226ff5e8c9377a
nina <ninagbs@abv.bg>
2023-06-14 17:22:46 +0300

    routes and express-setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes')
 
 
 
 app.use(express.static('public'));
 // parse body of forms and query strings
 app.use(express.urlencoded({extended: false}))
-app.get('/', (req, res)=>{
-    res.send(`<h1>Home page</h1>`)
-})
+
+
+app.use(routes)
 app.listen('5000', ()=>console.log('Server is listening on port 5000...'))
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 //add controller routes
-
+router.get('/', (req, res)=>{
+    res.send(`<h1>Home page</h1>`)
+})
 
 module.exports = router;
