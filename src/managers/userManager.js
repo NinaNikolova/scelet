2c847b8217ece1a57605085faadc5805341b10af
nina <ninagbs@abv.bg>
2023-06-15 09:48:37 +0300

    hash and compare password with bcrypt

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,7 +1,18 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt')
 
-exports.login = (username, password) => {
-
+exports.login = async (username, password) => {
+    //  find user by username
+    const user = await User.findOne({ username })
+    if (!user) {
+        throw new Error('Invalid user or password');
+    }
+    // check password
+    const isValid = await bcrypt.compare(password, user.password);
+    if(!isValid){
+        throw new Error('Invalid user or password');
+    }
+    
 }
 exports.register = async (userData) => {
     const user = await User.findOne({ username: userData.username })
